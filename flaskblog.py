143b8c96ea64336e5801da6d607155c0a9af6c68
Mert Kirman <mertkirman@Merts-MacBook-Air-3.local>
2025-07-30 17:09:03 +0300

    Create dummy data to show on home page. Create layout.html and use template inheritance. Integrate Bootstrap framework.

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# Sample data to simulate a database
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2025'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2025'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)    # Jinja templating engine allows us to pass variables to HTML templates
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')

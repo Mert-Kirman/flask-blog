9ae4914a6ae06717b97c68ef29e13f5b00de7866
Mert Kirman <mertkirman@Merts-MacBook-Air-3.local>
2025-07-31 15:42:03 +0300

    Create forms, accept and validate user input

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ddfddb28228b3c27a28ccb091828b419'
 
 # Sample data to simulate a database
 posts = [
@@ -19,9 +22,28 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
     return render_template('home.html', posts=posts)    # Jinja templating engine allows us to pass variables to HTML templates
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'john@doe.com' and form.password.data == 'johndoe':   # Fake data for simulating a successful login
+            flash(f'Logged in successfully!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)

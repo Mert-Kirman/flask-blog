8cc101b37f363b37a30e7ca6df05be2b1a19a46f
Mert Kirman <mertkirman@macbookair.local>
2025-08-01 15:14:10 +0300

    Create an authentication system for user login/logout

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ddfddb28228b3c27a28ccb091828b419'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.app_context().push()  # Ensure the app context is pushed for database operations
 
 db = SQLAlchemy(app)
-from flaskblog.models import User, Post # Import models to ensure they are registered with SQLAlchemy
-db.create_all()  # Create database tables if they do not exist
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # type: ignore
+login_manager.login_message_category = 'info'
 
 # Import routes after initializing the app and db to avoid circular imports.
 # Importing routes here allows the app to be aware of the routes defined in routes.py.
--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from flaskblog import app
+from flaskblog import app, db
+
+with app.app_context():
+    db.create_all()  # Create database tables if they do not exist
 
 if __name__ == '__main__':
     app.run(debug=True)
